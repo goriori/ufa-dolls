02fabef05b827b189f033fb2a194a3d0951cb668
goriori <vladislav.borisov2010@gmail.com>
2024-08-29 17:22:25 +0400

    change: - change  state success video modal

--- public/config.js
+++ public/config.js
@@ -3,6 +3,6 @@ window.LOCAL_API = 'http://localhost:3000'
 window.HOME = 'http://ufa-glav.local'
 window.CEF = false
 window.TIMEOUT = 90
-window.IS_DEV = false
+window.IS_DEV = true
 window.TIME_RECORD_SECONDS = 120
 window.LIST_MANUALS = ['manual/tail/tail_manual.mp4']
